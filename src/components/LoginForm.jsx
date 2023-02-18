039ea71a2e65131d398aad5ba4662912d30d5e17
Saul <phasecross@hotmail.com>
2023-02-17 22:13:42 -0700

    Add responsiveness, remove redundant divs

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -49,6 +49,7 @@ export const LoginForm = ({
       <div className="fondo">
         <Box className="backgroundWhiteSmoke">
           <Box className="copyright-box">
+            {/* ¿Tal vez el Copyright se ve mejor con el logo encima del texto? */}
             <Typography variant="h1">
               Desarrollado por &copy; CSI PRO 2023
             </Typography>
@@ -66,7 +67,7 @@ export const LoginForm = ({
             transform: "translate(-50%, -50%)",
             background: "white",
             borderRadius: { xs: "0", sm: "32px" },
-            boxShadow: "rgba(0, 0, 0, 0.24) 0 10px 20px",
+            boxShadow: { xs: "none", sm: "rgba(0, 0, 0, 0.24) 0 10px 20px" },
           }}
         >
           <CardContent>
@@ -82,20 +83,46 @@ export const LoginForm = ({
               <h2>Iniciar sesión</h2>
             </div>
 
-            <form onSubmit={handleSubmit}>
-              <div className="inputCorreo">
+            <form className="login-form" onSubmit={handleSubmit}>
+              {/* <div className="inputCorreo"> */}
+              <TextField
+                name="Inpt_email"
+                inputRef={correoRef}
+                type="email"
+                autoFocus
+                autoComplete="new-password"
+                placeholder="correo@email.com"
+                label="Correo electrónico"
+                required
+                id="email"
+                margin="dense"
+                error={error}
+                style={{ width: 340 }}
+                sx={{
+                  "& label.Mui-focused": {
+                    color: "#00C795",
+                  },
+                  "& .MuiInput-underline:after": {
+                    borderBottomColor: "#00C795",
+                  },
+                  "& .MuiOutlinedInput-root": {
+                    "& fieldset": {
+                      borderColor: "light",
+                    },
+                    "&:hover fieldset": {
+                      borderColor: "black",
+                    },
+                    "&.Mui-focused fieldset": {
+                      borderColor: "#00C795",
+                    },
+                  },
+                }}
+              />
+              {/* </div>   */}
+
+              {/* <div className="inputPass"> */}
+              <FormControl variant="outlined">
                 <TextField
-                  name="Inpt_email"
-                  inputRef={correoRef}
-                  type="email"
-                  autoFocus
-                  autoComplete="new-password"
-                  placeholder="correo@email.com"
-                  label="Correo electrónico"
-                  required
-                  id="email"
-                  margin="dense"
-                  error={error}
                   style={{ width: 340 }}
                   sx={{
                     "& label.Mui-focused": {
@@ -116,102 +143,81 @@ export const LoginForm = ({
                       },
                     },
                   }}
-                />
-              </div>
-
-              <div className="inputPass">
-                <FormControl variant="outlined">
-                  <TextField
-                    style={{ width: 340 }}
-                    sx={{
-                      "& label.Mui-focused": {
-                        color: "#00C795",
-                      },
-                      "& .MuiInput-underline:after": {
-                        borderBottomColor: "#00C795",
-                      },
-                      "& .MuiOutlinedInput-root": {
-                        "& fieldset": {
-                          borderColor: "light",
-                        },
-                        "&:hover fieldset": {
-                          borderColor: "black",
-                        },
-                        "&.Mui-focused fieldset": {
-                          borderColor: "#00C795",
-                        },
-                      },
-                    }}
-                    margin="dense"
-                    id="outlined-adornment-password"
-                    type={values.showPassword ? "text" : "password"}
-                    inputRef={passwordRef}
-                    label="Contraseña"
-                    variant="outlined"
-                    error={error}
-                    required
-                    helperText={errorMessage}
-                    placeholder="Contraseña"
-                    autoComplete="new-password"
-                    InputProps={{
-                      endAdornment: (
-                        <InputAdornment position="end">
-                          <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={handleClickShowPassword}
-                            edge="end"
-                          >
-                            {values.showPassword ? (
-                              <VisibilityOff />
-                            ) : (
-                              <Visibility />
-                            )}
-                          </IconButton>
-                        </InputAdornment>
-                      ),
-                    }}
-                  />
-                </FormControl>
-              </div>
-              <div className="checkBox" ref={checkRef} onChange={toggle}>
-                <FormControlLabel
-                  label={
-                    <span style={{ fontSize: "14px", color: "#333333" }}>
-                      {"Mantener sesión iniciada"}
-                    </span>
-                  }
-                  sx={{
-                    "& .MuiSvgIcon-root": { fontSize: 18, color: "#00C795" },
+                  margin="dense"
+                  id="outlined-adornment-password"
+                  type={values.showPassword ? "text" : "password"}
+                  inputRef={passwordRef}
+                  label="Contraseña"
+                  variant="outlined"
+                  error={error}
+                  required
+                  helperText={errorMessage}
+                  placeholder="Contraseña"
+                  autoComplete="new-password"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          edge="end"
+                        >
+                          {values.showPassword ? (
+                            <VisibilityOff />
+                          ) : (
+                            <Visibility />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
                   }}
-                  control={<Checkbox />}
                 />
-              </div>
-              <div className="confirmarDatos">
-                <Button
-                  style={{
-                    borderRadius: 13,
-                    backgroundColor: "#00C795",
-                    padding: "10px 32px",
-                    fontSize: "16px",
-                    textTransform: "none",
-                    fontWeight: "regular",
-                    width: 140,
-                    height: 45,
-                  }}
-                  variant="contained"
-                  size="large"
-                  id="Btn_login"
-                  type="submit"
-                >
-                  {"Siguiente"}
-                </Button>
-              </div>
-              <div className="recuperar">
-                <Link to="/recuperacion">¿Olvidaste tu contraseña?</Link>
-              </div>
-              <div className="ayuda">
-                <Link to="/ayuda">¿Problemas para iniciar sesión?</Link>
-              </div>
+              </FormControl>
+              {/* </div> */}
+              {/* <div className="checkBox" ref={checkRef} onChange={toggle}> */}
+              <FormControlLabel
+                label={
+                  <span style={{ fontSize: "16px", color: "#333333" }}>
+                    Mantener sesión iniciada
+                  </span>
+                }
+                sx={{
+                  "& .MuiSvgIcon-root": { fontSize: "24px", color: "#00C795" },
+                }}
+                control={<Checkbox onChange={toggle} />}
+              />
+              {/* </div> */}
+              {/* <div className="confirmarDatos"> */}
+              <Button
+                sx={{
+                  borderRadius: "13px",
+                  backgroundColor: "#00C795",
+                  padding: "10px 32px",
+                  fontSize: "16px",
+                  textTransform: "none",
+                  fontWeight: "regular",
+                  margin: "auto",
+                  width: 140,
+                  height: 45,
+                }}
+                variant="contained"
+                size="large"
+                id="Btn_login"
+                type="submit"
+              >
+                Siguiente
+              </Button>
+              {/* </div> */}
+              {/* <div className="recuperar"> */}
+              <Link className="recuperar" to="/recuperacion">
+                ¿Olvidaste tu contraseña?
+              </Link>
+              {/* </div> */}
+              {/* <div className="ayuda"> */}
+              <Link className="ayuda" to="/ayuda">
+                ¿Problemas para iniciar sesión?
+              </Link>
+              {/* </div> */}
             </form>
           </CardContent>
         </Card>
