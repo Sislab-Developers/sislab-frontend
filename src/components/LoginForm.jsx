0ecd0b524947b43f4eb83e343c7058bfae8a6c6e
ruizaul <fimbressaul@hotmail.com>
2023-02-20 23:28:37 -0700

    Quitar checkbox de sesion

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,23 +1,15 @@
-import { useState } from "react";
-import IconButton from "@mui/material/IconButton";
-import InputAdornment from "@mui/material/InputAdornment";
-import FormControl from "@mui/material/FormControl";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import TextField from "@mui/material/TextField";
-import Logo from "../assets/img/logoLogin.svg";
-import LogoCSIPRO from "../assets/img/logo-csipro.png";
+import { useState } from 'react';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import TextField from '@mui/material/TextField';
+import Logo from '../assets/img/logoLogin.svg';
+import LogoCSIPRO from '../assets/img/logo-csipro.png';
 
-import { Link } from "react-router-dom";
-import {
-  FormControlLabel,
-  Checkbox,
-  Button,
-  Box,
-  Typography,
-  Card,
-  CardContent,
-} from "@mui/material";
+import { Link } from 'react-router-dom';
+import { Button, Box, Typography, Card, CardContent } from '@mui/material';
 
 export const LoginForm = ({
   handleSubmit,
@@ -27,7 +19,7 @@ export const LoginForm = ({
   errorMessage,
 }) => {
   const [values, setValues] = useState({
-    password: "",
+    password: '',
     showPassword: false,
   });
 
@@ -54,14 +46,14 @@ export const LoginForm = ({
         <Card
           variant="outlined"
           sx={{
-            position: "absolute",
-            width: { xs: "100%", sm: "380px" },
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            background: "white",
-            borderRadius: { xs: "48px 48px 0 0", sm: "32px" },
-            boxShadow: { xs: "none", sm: "rgba(0, 0, 0, 0.24) 0 10px 20px" },
+            position: 'absolute',
+            width: { xs: '100%', sm: '380px' },
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            background: 'white',
+            borderRadius: { xs: '48px 48px 0 0', sm: '32px' },
+            boxShadow: { xs: 'none', sm: 'rgba(0, 0, 0, 0.24) 0 10px 20px' },
           }}
         >
           <CardContent>
@@ -89,21 +81,21 @@ export const LoginForm = ({
                 error={error}
                 style={{ width: 340 }}
                 sx={{
-                  "& label.Mui-focused": {
-                    color: "#00C795",
+                  '& label.Mui-focused': {
+                    color: '#00C795',
                   },
-                  "& .MuiInput-underline:after": {
-                    borderBottomColor: "#00C795",
+                  '& .MuiInput-underline:after': {
+                    borderBottomColor: '#00C795',
                   },
-                  "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                      borderColor: "light",
+                  '& .MuiOutlinedInput-root': {
+                    '& fieldset': {
+                      borderColor: 'light',
                     },
-                    "&:hover fieldset": {
-                      borderColor: "black",
+                    '&:hover fieldset': {
+                      borderColor: 'black',
                     },
-                    "&.Mui-focused fieldset": {
-                      borderColor: "#00C795",
+                    '&.Mui-focused fieldset': {
+                      borderColor: '#00C795',
                     },
                   },
                 }}
@@ -113,27 +105,27 @@ export const LoginForm = ({
                 <TextField
                   style={{ width: 340 }}
                   sx={{
-                    "& label.Mui-focused": {
-                      color: "#00C795",
+                    '& label.Mui-focused': {
+                      color: '#00C795',
                     },
-                    "& .MuiInput-underline:after": {
-                      borderBottomColor: "#00C795",
+                    '& .MuiInput-underline:after': {
+                      borderBottomColor: '#00C795',
                     },
-                    "& .MuiOutlinedInput-root": {
-                      "& fieldset": {
-                        borderColor: "light",
+                    '& .MuiOutlinedInput-root': {
+                      '& fieldset': {
+                        borderColor: 'light',
                       },
-                      "&:hover fieldset": {
-                        borderColor: "black",
+                      '&:hover fieldset': {
+                        borderColor: 'black',
                       },
-                      "&.Mui-focused fieldset": {
-                        borderColor: "#00C795",
+                      '&.Mui-focused fieldset': {
+                        borderColor: '#00C795',
                       },
                     },
                   }}
                   margin="dense"
                   id="outlined-adornment-password"
-                  type={values.showPassword ? "text" : "password"}
+                  type={values.showPassword ? 'text' : 'password'}
                   inputRef={passwordRef}
                   label="Contraseña"
                   variant="outlined"
@@ -162,27 +154,15 @@ export const LoginForm = ({
                 />
               </FormControl>
 
-              <FormControlLabel
-                label={
-                  <span style={{ fontSize: "16px", color: "#333333" }}>
-                    Mantener sesión iniciada
-                  </span>
-                }
-                sx={{
-                  "& .MuiSvgIcon-root": { fontSize: "24px", color: "#00C795" },
-                }}
-                control={<Checkbox />}
-              />
-
               <Button
                 sx={{
-                  borderRadius: "13px",
-                  backgroundColor: "#00C795",
-                  padding: "10px 32px",
-                  fontSize: "16px",
-                  textTransform: "none",
-                  fontWeight: "regular",
-                  margin: "auto",
+                  borderRadius: '13px',
+                  backgroundColor: '#00C795',
+                  padding: '10px 32px',
+                  fontSize: '16px',
+                  textTransform: 'none',
+                  fontWeight: 'regular',
+                  margin: 'auto',
                   width: 140,
                   height: 45,
                 }}
